ba447ce219cd309d525b172e3d133570ea82ad88
Xiaokailnol <2519840456@qq.com>
2026-01-08 22:37:11 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -62,7 +62,7 @@ clone_repo $openwrt_pkg_repo master openwrt_pkg_ma &
 clone_repo $openwrt_add_repo master OpenWrt-Add &
 clone_repo $dockerman_repo master dockerman &
 clone_repo $docker_lib_repo master docker_lib &
-clone_repo $argon_repo master argon_repo &
+clone_repo $argon_repo openwrt-25.12 argon_repo &
 # 等待所有后台任务完成
 wait
 
