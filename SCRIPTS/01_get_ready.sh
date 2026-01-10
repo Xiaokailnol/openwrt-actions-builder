4fff82b18418f2658a24ed2fa3a352ec20079b9d
Xiaokailnol <2519840456@qq.com>
2026-01-10 11:10:30 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -47,7 +47,6 @@ sbw_pkg_repo="https://github.com/sbwml/openwrt_pkgs"
 natmap_repo="https://github.com/blueberry-pie-11/luci-app-natmap"
 xwrt_repo="https://github.com/QiuSimons/openwrt-natflow"
 argon_repo="https://gitea.kejizero.xyz/zhao/luci-theme-argon"
-openwrt_file="https://github.com/Xiaokailnol/openwrt-actions-builder"
 
 # 开始克隆仓库，并行执行
 clone_repo $openwrt_repo $latest_release openwrt &
@@ -64,7 +63,6 @@ clone_repo $openwrt_add_repo master OpenWrt-Add &
 clone_repo $dockerman_repo master dockerman &
 clone_repo $docker_lib_repo master docker_lib &
 clone_repo $argon_repo openwrt-25.12 argon_repo &
-clone_repo $openwrt_file openwrt-25.12 openwrt_file &
 # 等待所有后台任务完成
 wait
 
@@ -79,7 +77,6 @@ cp -rf ./openwrt_snap/feeds.conf.default ./openwrt/feeds.conf.default
 rm -rf ./OpenWrt-Add/{luci-app-argon-config,luci-theme-argon}
 rm -rf ./OpenWrt-Add/openwrt_pkgs/luci-app-adguardhome
 cp -rf ./argon_repo/* ./OpenWrt-Add/
-cp -rf ./openwrt_file/PATCH ./PATCH/
 
 # 修改默认源地址
 sed -i 's,mirror.sjtu.edu.cn/openwrt,openwrt.kejizero.xyz,g' ./OpenWrt-Add/addition-trans-zh/files/zzz-default-settings
